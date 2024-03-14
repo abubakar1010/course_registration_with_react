e32f4204c26a683eaac3243707d95227547cfb91
Abubakar1010 <abubakar850772@gmail.com>
2024-03-14 07:03:03 +0600

    project setup with react and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-4xl bg-cyan-600 p-5'>Project setup with react and tailwind</h1>
+    </>
+  )
+}
+
+export default App
